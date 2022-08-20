51db53e5e35b709e6f8a3bd5b63cd23ddaa97cef
Daniel Lamando <dan@danopia.net>
2022-08-20 23:38:53 +0000

    Improve React entrypoint and prep routing

--- client/main.tsx
+++ client/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { createRoot } from 'react-dom/client';
 import { Meteor } from 'meteor/meteor';
-import { render } from 'react-dom';
 
 import { App } from '/imports/ui/App';
 import './install-worker';
 
 Meteor.startup(() => {
-  render(<App />, document.getElementById('react-target'));
+  const container = document.getElementById('react-target');
+  if (!container) throw new Error('Root element not found');
+  const root = createRoot(container);
+  root.render(<App />);
 });
--- imports/ui/App.tsx
+++ imports/ui/App.tsx
@@ -1,9 +1,38 @@
 import React from 'react';
+import { useRoutes } from 'raviger';
+import { ErrorBoundary, FallbackProps } from 'react-error-boundary';
+
 import { ActivityShell } from './ActivityShell';
 import { RuntimeProvider } from './RuntimeProvider';
 
-export const App = () => (
-  <RuntimeProvider>
-    <ActivityShell />
-  </RuntimeProvider>
+const ErrorFallback = ({ error, resetErrorBoundary }: FallbackProps) => (
+  <div role="alert">
+    <p>Something went wrong:</p>
+    <pre>{error.message}</pre>
+    <button onClick={resetErrorBoundary}>Reset page</button>
+  </div>
 );
+
+const routes = {
+  '/shell': () => <ActivityShell />,
+};
+
+export const App = () => {
+  const route = useRoutes(routes) || (
+    <section>
+      <h2>Page Not Found</h2>
+      <p>
+        Like, I didn&apos;t know what you were looking for. No route matched the URL.
+        Check the link that sent you here.
+      </p>
+    </section>
+  );
+
+  return (
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
+      <RuntimeProvider>
+        {route}
+      </RuntimeProvider>
+    </ErrorBoundary>
+  );
+};
