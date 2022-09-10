b100caa9b6fdffb6c63e314b7b506d5d48a30305
Daniel Lamando <dan@danopia.net>
2022-09-10 15:07:00 +0000

    Comment out now-unused network catalog code
    Catalogs are currently fully local.
    The server is literally just an HTTP proxy at this point.
    This will not stay true for long.

--- imports/runtime/SessionCatalog.ts
+++ imports/runtime/SessionCatalog.ts
@@ -1,89 +1,89 @@
-import { Mongo } from "meteor/mongo";
-import { EntitiesCollection } from "../db/entities";
-import { ArbitraryEntity } from "../entities/core";
+// import { Mongo } from "meteor/mongo";
+// import { EntitiesCollection } from "../db/entities";
+// import { ArbitraryEntity } from "../entities/core";
 
-export class SessionCatalog {
-  constructor(syncedCatalogId?: string) {
-    if (syncedCatalogId) {
-      this.catalogId = syncedCatalogId;
-      this.coll = EntitiesCollection;
-    } else {
-      this.catalogId = undefined;
-      this.coll = new Mongo.Collection(null);
-    }
-  }
+// export class SessionCatalog {
+//   constructor(syncedCatalogId?: string) {
+//     if (syncedCatalogId) {
+//       this.catalogId = syncedCatalogId;
+//       this.coll = EntitiesCollection;
+//     } else {
+//       this.catalogId = undefined;
+//       this.coll = new Mongo.Collection(null);
+//     }
+//   }
 
-  private readonly catalogId: string | undefined;
-  private readonly coll: typeof EntitiesCollection;
+//   private readonly catalogId: string | undefined;
+//   private readonly coll: typeof EntitiesCollection;
 
-  insertEntity<T extends ArbitraryEntity>(entity: T) {
-    this.coll.insert({
-      ...entity,
-      metadata: {
-        ...entity.metadata,
-        catalogId: this.catalogId,
-        generation: 1,
-      },
-    });
-  }
+//   insertEntity<T extends ArbitraryEntity>(entity: T) {
+//     this.coll.insert({
+//       ...entity,
+//       metadata: {
+//         ...entity.metadata,
+//         catalogId: this.catalogId,
+//         generation: 1,
+//       },
+//     });
+//   }
 
-  findEntities<T extends ArbitraryEntity>(apiVersion: T["apiVersion"], kind: T["kind"]) {
-    return this.coll.find({
-      apiVersion: apiVersion as any,
-      kind: kind as any,
-      'metadata.catalogId': this.catalogId,
-    }) as Mongo.Cursor<T & { _id: string }>;
-  }
+//   findEntities<T extends ArbitraryEntity>(apiVersion: T["apiVersion"], kind: T["kind"]) {
+//     return this.coll.find({
+//       apiVersion: apiVersion as any,
+//       kind: kind as any,
+//       'metadata.catalogId': this.catalogId,
+//     }) as Mongo.Cursor<T & { _id: string }>;
+//   }
 
-  getEntity<T extends ArbitraryEntity>(apiVersion: T["apiVersion"], kind: T["kind"], namespace: string | undefined, name: string) {
-    return this.coll.findOne({
-      apiVersion: apiVersion as any,
-      kind: kind as any,
-      'metadata.catalogId': this.catalogId,
-      'metadata.namespace': namespace,
-      'metadata.name': name,
-    }) as T & { _id: string };
-  }
+//   getEntity<T extends ArbitraryEntity>(apiVersion: T["apiVersion"], kind: T["kind"], namespace: string | undefined, name: string) {
+//     return this.coll.findOne({
+//       apiVersion: apiVersion as any,
+//       kind: kind as any,
+//       'metadata.catalogId': this.catalogId,
+//       'metadata.namespace': namespace,
+//       'metadata.name': name,
+//     }) as T & { _id: string };
+//   }
 
-  updateEntity<T extends ArbitraryEntity>(newEntity: T) {
-    if (!newEntity.metadata.generation) throw new Error(`BUG: no generation in update`);
-    const count = this.coll.update({
-      apiVersion: newEntity.apiVersion as any,
-      kind: newEntity.kind as any,
-      'metadata.catalogId': this.catalogId,
-      'metadata.namespace': newEntity.metadata.namespace,
-      'metadata.name': newEntity.metadata.name,
-      'metadata.generation': newEntity.metadata.generation,
-    }, {
-      ...(newEntity as (ArbitraryEntity & {_id: string})),
-      metadata: {
-        ...newEntity.metadata,
-        generation: (newEntity.metadata.generation ?? 0) + 1,
-      },
-    });
-    if (count == 0) throw new Error(`TODO: Update applied to zero entities`);
-  }
+//   updateEntity<T extends ArbitraryEntity>(newEntity: T) {
+//     if (!newEntity.metadata.generation) throw new Error(`BUG: no generation in update`);
+//     const count = this.coll.update({
+//       apiVersion: newEntity.apiVersion as any,
+//       kind: newEntity.kind as any,
+//       'metadata.catalogId': this.catalogId,
+//       'metadata.namespace': newEntity.metadata.namespace,
+//       'metadata.name': newEntity.metadata.name,
+//       'metadata.generation': newEntity.metadata.generation,
+//     }, {
+//       ...(newEntity as (ArbitraryEntity & {_id: string})),
+//       metadata: {
+//         ...newEntity.metadata,
+//         generation: (newEntity.metadata.generation ?? 0) + 1,
+//       },
+//     });
+//     if (count == 0) throw new Error(`TODO: Update applied to zero entities`);
+//   }
 
-  // Mutation helper
-  mutateEntity<T extends ArbitraryEntity>(apiVersion: T["apiVersion"], kind: T["kind"], namespace: string | undefined, name: string, mutationCb: (x: T) => void | Symbol) {
-    const entity = this.getEntity(apiVersion, kind, namespace, name);
-    if (!entity) throw new Error(`Entity doesn't exist`);
+//   // Mutation helper
+//   mutateEntity<T extends ArbitraryEntity>(apiVersion: T["apiVersion"], kind: T["kind"], namespace: string | undefined, name: string, mutationCb: (x: T) => void | Symbol) {
+//     const entity = this.getEntity(apiVersion, kind, namespace, name);
+//     if (!entity) throw new Error(`Entity doesn't exist`);
 
-    const result = mutationCb(entity);
-    if (result == Symbol.for('no-op')) return;
+//     const result = mutationCb(entity);
+//     if (result == Symbol.for('no-op')) return;
 
-    this.updateEntity(entity);
-  }
+//     this.updateEntity(entity);
+//   }
 
-  deleteEntity<T extends ArbitraryEntity>(apiVersion: T["apiVersion"], kind: T["kind"], namespace: string | undefined, name: string) {
-    const count = this.coll.remove({
-      apiVersion: apiVersion,
-      kind: kind,
-      'metadata.catalogId': this.catalogId,
-      'metadata.namespace': namespace,
-      'metadata.name': name,
-    });
-    if (count == 0) throw new Error(`TODO: Delete applied to zero entities`);
-  }
+//   deleteEntity<T extends ArbitraryEntity>(apiVersion: T["apiVersion"], kind: T["kind"], namespace: string | undefined, name: string) {
+//     const count = this.coll.remove({
+//       apiVersion: apiVersion,
+//       kind: kind,
+//       'metadata.catalogId': this.catalogId,
+//       'metadata.namespace': namespace,
+//       'metadata.name': name,
+//     });
+//     if (count == 0) throw new Error(`TODO: Delete applied to zero entities`);
+//   }
 
-}
+// }
--- server/v1alpha1/api.ts
+++ server/v1alpha1/api.ts
@@ -1,9 +1,6 @@
 import { check } from "meteor/check";
 import { Meteor } from "meteor/meteor";
-import { upsertEntity } from "./catalog-upload";
 import { fetchRequestEntity } from "./fetch";
-import { EntitiesCollection } from "/imports/db/entities";
-import { Entity } from "/imports/entities";
 import { FetchRequestEntity } from "/imports/entities/protocol";
 
 Meteor.methods({
@@ -14,25 +11,24 @@ Meteor.methods({
     return await fetchRequestEntity(req);
   },
 
+  // async '/CatalogUpload/UpsertEntity'(catalogId: unknown, namespaceOverride: unknown, entity: Entity) {
+  //   check(catalogId, String);
+  //   check(namespaceOverride, String);
+  //   check(entity, Object);
+  //   check(entity['apiVersion'], String);
+  //   check(entity['kind'], String);
+  //   return await upsertEntity(catalogId, namespaceOverride, entity);
+  // },
 
-  async '/CatalogUpload/UpsertEntity'(catalogId: unknown, namespaceOverride: unknown, entity: Entity) {
-    check(catalogId, String);
-    check(namespaceOverride, String);
-    check(entity, Object);
-    check(entity['apiVersion'], String);
-    check(entity['kind'], String);
-    return await upsertEntity(catalogId, namespaceOverride, entity);
-  },
-
-  async '/CatalogUpload/RemoveOtherEntities'(catalogId: unknown, namespaceOverride: unknown, entityIds: string[]) {
-    check(catalogId, String);
-    check(namespaceOverride, String);
-    check(entityIds, Array);
-    const removedCount = EntitiesCollection.remove({
-      _id: { $nin: entityIds },
-      'metadata.catalogId': catalogId,
-      'metadata.namespace': namespaceOverride,
-    });
-    return removedCount;
-  },
+  // async '/CatalogUpload/RemoveOtherEntities'(catalogId: unknown, namespaceOverride: unknown, entityIds: string[]) {
+  //   check(catalogId, String);
+  //   check(namespaceOverride, String);
+  //   check(entityIds, Array);
+  //   const removedCount = EntitiesCollection.remove({
+  //     _id: { $nin: entityIds },
+  //     'metadata.catalogId': catalogId,
+  //     'metadata.namespace': namespaceOverride,
+  //   });
+  //   return removedCount;
+  // },
 });
