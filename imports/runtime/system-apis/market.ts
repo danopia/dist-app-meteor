7f7efd5ee42b1c39aadd914de6e3d289a55419b5
Daniel Lamando <dan@danopia.net>
2023-04-16 00:13:17 +0200

    Refactor market api to fix uninstalled bundled apps

--- imports/runtime/system-apis/market.ts
+++ imports/runtime/system-apis/market.ts
@@ -1,103 +1,89 @@
 import { stripIndent } from "common-tags";
-import { FetchRpcHandler } from "../FetchRpcHandler";
+
+import { FetchRpcHandler } from "/imports/runtime/FetchRpcHandler";
+import { EntityEngine } from "/imports/engine/EntityEngine";
 import { StaticCatalogs } from "/imports/engine/StaticCatalogs";
-import { ApplicationEntity } from "/imports/entities/manifest";
+
+import { ArbitraryEntity } from "/imports/entities/core";
+import { ApplicationEntity, IconSpec } from "/imports/entities/manifest";
 import { AppInstallationEntity } from "/imports/entities/profile";
 import { FetchRequestEntity, FetchResponseEntity } from "/imports/entities/protocol";
 
 /**
- * # session.v1alpha1.dist.app
+ * # market.v1alpha1.dist.app
  *
  * This API is used by:
  *   - market (bundled app)
  *       This API provides the network and runtime access
  *       to explore the available applications and any existing installations.
  */
-export async function serveMarketApi(rpc: {request: FetchRequestEntity['spec'], path: string, context: FetchRpcHandler }): Promise<FetchResponseEntity['spec']> {
-  console.log('MARKET:', rpc);
+export async function serveMarketApi(rpc: {
+  request: FetchRequestEntity['spec'],
+  path: string,
+  context: FetchRpcHandler,
+}): Promise<FetchResponseEntity['spec']> {
+  console.log('MARKET API:', rpc);
 
   if (rpc.path == 'list-available-apps' && rpc.request.method == 'GET') {
 
-    // Find places where we can find the user's available applications
-    const manifestNamespaces = Array
-      .from(rpc.context.runtime
-        .getNamespacesServingApi({
-          apiVersion: 'manifest.dist.app/v1alpha1',
-          kind: 'Application',
-          op: 'Read',
-        })
-        .keys());
+    // Find the user's available applications
+    const applications = findAllEntities<ApplicationEntity>(rpc.context.runtime,
+      'manifest.dist.app/v1alpha1', 'Application');
 
-    // Find existing applications
-    const applications = manifestNamespaces
-      .flatMap(x => rpc.context.runtime
-        .listEntities<ApplicationEntity>(
-          'manifest.dist.app/v1alpha1', 'Application', x)
-        .map(entity => ({ ns: x, entity })));
+    // Find the user's app installations
+    const installations = findAllEntities<AppInstallationEntity>(rpc.context.runtime,
+      'profile.dist.app/v1alpha1', 'AppInstallation');
 
 
-    // Find places where we can find the user's existing installations
-    const profileNamespaces = Array
-      .from(rpc.context.runtime
-        .getNamespacesServingApi({
-          apiVersion: 'profile.dist.app/v1alpha1',
-          kind: 'AppInstallation',
-          op: 'Read',
-        })
-        .keys());
-
-    // Find existing installations
-    const installations = profileNamespaces
-      .flatMap(x => rpc.context.runtime
-        .listEntities<AppInstallationEntity>(
-          'profile.dist.app/v1alpha1', 'AppInstallation', x)
-        .map(entity => ({ ns: x, entity })));
-
-    console.log('static', [...StaticCatalogs.entries()][0]);
-    console.log('dynamic', applications[0]);
-    console.log({installations})
-
-    const bundledApps = applications.map(({ns, entity: appRes}) => {
-      // const appRes: ApplicationEntity | undefined = catalog.flatMap(x => x.kind == 'Application' ? [x] : [])[0];
-
-      // Know where the app is already present
-      const appInstalls = installations.filter(x => {
-        return x.entity.spec.appUri == 'bundled:'+encodeURIComponent(ns);
-      });
-      console.log({appInstalls})
-
+    const bundledApps = Array.from(StaticCatalogs.entries()).map(([id, catalog]) => {
+      const appRes: ApplicationEntity | undefined = catalog.flatMap(x => x.kind == 'Application' ? [x] : [])[0];
       return {
-        id: 'bundled:'+ns.slice(4),
-        // url: 'bundled:'+encodeURIComponent(ns),
-        url: `entity://bundled/manifest.dist.app@v1alpha1/Application/${encodeURIComponent(ns)}`,
-        title: appRes.metadata.title ?? 'N/A',
-        description: appRes.metadata.description ?? 'N/A',
-        iconUrl: appRes.spec.brandImageUrl ?? (appRes.spec.icon?.type == 'glyph'
-        ? `data:image/svg+xml,${encodeURIComponent(stripIndent`
-            <svg width="800" height="600" version="1.1" viewBox="0 0 80 60" xmlns="http://www.w3.org/2000/svg">
-              <circle cx="40" cy="30" r="25" fill="${appRes.spec.icon.glyph.backgroundColor}" />
-              <text x="40" y="1.5em" font-size="25" text-anchor="middle">${appRes.spec.icon.glyph.text}</text>
-            </svg>`)}`
-        : appRes.spec.icon?.type == 'svg'
-        ? `data:image/svg+xml,${encodeURIComponent(stripIndent`
-            <svg width="800" height="600" version="1.1" viewBox="0 0 80 60" xmlns="http://www.w3.org/2000/svg">
-              <circle cx="40" cy="30" r="25" fill="${appRes.spec.icon.svg.backgroundColor}" />
-              <image x="25" y="15" width="30" height="30" href="data:image/svg+xml,${encodeURIComponent(appRes.spec.icon.svg.textData)}" />
-            </svg>`)}`
-        : null),
-        currentInstallations: appInstalls.map(x => ({
-          profileNamespace: x.ns,
-          appInstallName: x.entity.metadata.name,
-        })),
+        id: 'bundled:'+id.slice(4),
+        url: `entity://bundled/manifest.dist.app@v1alpha1/Application/${encodeURIComponent(id)}`,
+        appUri: 'bundled:'+encodeURIComponent(id),
+        appRes,
       };
     });
+
+    const discoveredApps = applications.map(({ns, entity: appRes}) => {
+      return {
+        id: 'bundled:'+ns.slice(4),
+        url: `entity://bundled/manifest.dist.app@v1alpha1/Application/${encodeURIComponent(ns)}`,
+        appUri: 'bundled:'+encodeURIComponent(ns),
+        appRes,
+      };
+    });
+
+    const seenAppUrls = new Set<string>(bundledApps.map(x => x.url));
+    const allApps = [
+      ...bundledApps,
+      ...discoveredApps.filter(x => !seenAppUrls.has(x.url)),
+    ]
+      .map(appInfo => {
+        // Know where the app is already present
+        const appInstalls = installations.filter(x => {
+          return x.entity.spec.appUri == appInfo.appUri;
+        });
+
+        return {
+          id: appInfo.id,
+          url: appInfo.url,
+          title: appInfo.appRes.metadata.title ?? 'N/A',
+          description: appInfo.appRes.metadata.description ?? 'N/A',
+          iconUrl: brandImageUrlForApp(appInfo.appRes),
+          currentInstallations: appInstalls.map(x => ({
+            profileNamespace: x.ns,
+            appInstallName: x.entity.metadata.name,
+          })),
+        };
+      });
 
     return {
       status: 200,
       headers: [
         ['content-type', 'application/json'],
       ],
-      // bodyStrea: (async function*() {
+      // bodyStream: (async function*() {
       //   return '';
       // }),
       body: JSON.stringify({
@@ -108,10 +94,7 @@ export async function serveMarketApi(rpc: {request: FetchRequestEntity['spec'], 
           namespace: 'profile:guest',
           title: 'Guest session',
         }],
-        listings: [
-          ...bundledApps,
-          // TODO: other app sources
-        ],
+        listings: allApps,
       }),
     };
   }
@@ -120,4 +103,54 @@ export async function serveMarketApi(rpc: {request: FetchRequestEntity['spec'], 
     status: 201,
     body: 'ok',
   };
+}
+
+function brandImageUrlForApp(appRes: ApplicationEntity) {
+  if (appRes.spec.brandImageUrl) return appRes.spec.brandImageUrl;
+  if (appRes.spec.icon) return brandImageUrlForIcon(appRes.spec.icon);
+  return null;
+}
+
+function brandImageUrlForIcon(icon: IconSpec) {
+  switch (icon.type) {
+    case 'glyph': {
+      return `data:image/svg+xml,${encodeURIComponent(stripIndent`
+        <svg width="800" height="600" version="1.1" viewBox="0 0 80 60" xmlns="http://www.w3.org/2000/svg">
+          <circle cx="40" cy="30" r="25" fill="${icon.glyph.backgroundColor}" />
+          <text x="40" y="1.5em" font-size="25" text-anchor="middle">${icon.glyph.text}</text>
+        </svg>`)}`;
+    }
+    case 'svg': {
+      return `data:image/svg+xml,${encodeURIComponent(stripIndent`
+        <svg width="800" height="600" version="1.1" viewBox="0 0 80 60" xmlns="http://www.w3.org/2000/svg">
+          <circle cx="40" cy="30" r="25" fill="${icon.svg.backgroundColor}" />
+          <image x="25" y="15" width="30" height="30" href="data:image/svg+xml,${encodeURIComponent(icon.svg.textData)}" />
+        </svg>`)}`
+    }
+    default: {
+      return null;
+    }
+  }
+}
+
+// TODO: move a shared file
+function findAllEntities<T extends ArbitraryEntity>(
+  runtime: EntityEngine,
+  apiVersion: T["apiVersion"],
+  kind: T["kind"],
+) {
+  // Find places where we can find the type of entity
+  const namespaces = Array
+    .from(runtime
+      .getNamespacesServingApi({
+        apiVersion, kind,
+        op: 'Read',
+      })
+      .keys());
+
+  // Collect all of the entities
+  return namespaces
+    .flatMap(x => runtime
+      .listEntities<T>(apiVersion, kind, x)
+      .map(entity => ({ ns: x, entity })));
 }
