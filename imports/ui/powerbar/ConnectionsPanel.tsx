dc413a762525a40355599cacbc8ff2ecd356bf96
Daniel Lamando <dan@danopia.net>
2023-09-04 22:24:21 +0200

    chore(powerbar): Design connection panel better

--- imports/ui/powerbar/ConnectionsPanel.tsx
+++ imports/ui/powerbar/ConnectionsPanel.tsx
@@ -9,12 +9,14 @@ export const ConnectionsPanel = () => {
     const allConns = [{
       label: Meteor.absoluteUrl(),
       reconnect: () => Meteor.reconnect(),
+      disconnect: () => Meteor.disconnect(),
       status: Meteor.status(),
     }];
     for (const [url, conn] of remoteConns.entries()) {
       allConns.push({
         label: url,
         reconnect: () => conn.reconnect(),
+        disconnect: () => conn.disconnect(),
         status: conn.status(),
       });
     }
@@ -24,9 +26,16 @@ export const ConnectionsPanel = () => {
   return (<>
     {connections.map((conn, connIdx) => (
       <li key={connIdx}>
-        <div title={conn.label}>srv{connIdx}</div>
-        <div style={{fontSize: '0.6em'}}>{conn.status.status}</div>
-        <button style={{fontSize: '0.6em', padding: '0.2em 0', display: 'block', width: '100%'}} type="button" disabled={conn.status.connected} onClick={conn.reconnect}>reconnect</button>
+        <div style={{fontSize: '0.7em'}} title={conn.label}>{new URL(conn.label).host}</div>
+        <div style={{fontSize: '0.6em', margin: '1px 0 3px'}}>
+          {conn.status.connected ? (
+            <div style={{width: '8px', aspectRatio: '1', borderRadius: '50%', backgroundColor: 'green', display: 'inline-block', verticalAlign: 'middle', marginRight: '0.3em'}} />
+          ) : []}
+          {conn.status.status}</div>
+          <div style={{display: 'flex', gap: '0.2em'}}>
+            <button style={{fontSize: '0.6em', padding: '0.2em 0', display: 'block', flex: 1}} type="button" disabled={conn.status.connected} onClick={conn.reconnect}>reconnect</button>
+            <button style={{fontSize: '0.6em', padding: '0.2em 0', display: 'block', aspectRatio: '1'}} type="button" disabled={conn.status.status == 'offline'} onClick={conn.disconnect}>×</button>
+          </div>
       </li>
     ))}
   </>);
